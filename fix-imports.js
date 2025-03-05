effafcbc0a06a886b6f67bfdc65de2cf66eb9f19
Ramiro Cerdá <rami992009@gmail.com>
2025-03-04 23:00:41 -0300

    chore(config): deploy

--- fix-imports.js
+++ fix-imports.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name properly in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Directory to scan for TypeScript files
 const srcDir = path.join(__dirname, "src");
